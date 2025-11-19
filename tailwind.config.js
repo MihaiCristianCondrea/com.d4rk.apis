5e755da8d1579a2020d15ee92e96806e713eedd2
D4rK7355608 <d4rk7355608@gmail.com>
2025-11-19 22:20:07 +0200

    fix........

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/main/**/*.{js,html}', './pages/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'surface-container-lowest': 'var(--md-sys-color-surface-container-lowest)',
+        'surface-container-low': 'var(--md-sys-color-surface-container-low)',
+        'surface-container': 'var(--md-sys-color-surface-container)',
+        'surface-container-high': 'var(--md-sys-color-surface-container-high)',
+        'surface-container-highest': 'var(--md-sys-color-surface-container-highest)',
+      },
+    },
   },
   plugins: [],
 };
