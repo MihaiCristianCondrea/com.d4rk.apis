5886ff218bafd48e598b2d42fb176cb966482d27
D4rK7355608 <d4rk7355608@gmail.com>
2025-11-19 20:58:33 +0200

    tail thing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   root: '.',
   base: './',
   publicDir: resolve(__dirname, 'src/main/assets'),
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src/main'),
