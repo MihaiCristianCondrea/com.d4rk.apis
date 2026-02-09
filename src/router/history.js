d407dca396cb210557ee3fda60513cfd375a9a0b
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2026-02-09 21:08:10 +0200

    refactor: converge on feature-route router flow

--- src/router/history.js
+++
@@ -1,2 +0,0 @@
-/** History helper for route transitions. */
-export function pushRoute(routeId) { window.history.pushState({ page: `#${routeId}` }, '', `#${routeId}`); }
--- src/routes/android-studio-tutorials.route.js
+++
@@ -1,3 +0,0 @@
-/** Route module for android-studio-tutorials. */
-import * as page from '../pages/android-studio-tutorials/page.js';
-export default { id: 'android-studio-tutorials-api', path: '#android-studio-tutorials-api', title: 'android-studio-tutorials', page };
